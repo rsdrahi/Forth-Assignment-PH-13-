f78e82d8ff71ee266136efda95c7c0bc4bda9688
rsdrahi <rsdrahi13@gmail.com>
2026-02-24 02:02:45 +0600

    work while interview-btn and rejected-btn click count the total in interview and rejected in the header also delete the job card while click the delete icon. also style some status button outline.

--- script/scritpt.js
+++ script/scritpt.js
@@ -4,7 +4,9 @@ let totalCount = document.getElementById('total-count');
 let interviewCount = document.getElementById('interview-count');
 let rejectedCount = document.getElementById('rejected-count');
 
+// job total count
 const jobsSectionCard = document.getElementById('jobs-section');
+// console.log(jobsSectionCard.children)
 // console.log(jobsSectionCard.children.length);
 
 function allTotalCount () {
@@ -13,26 +15,32 @@ function allTotalCount () {
 
 allTotalCount();
 
+// card status change
+
 jobsSectionCard.addEventListener("click", function (event) {
+  // console.log('clicked')
   const cards = event.target.closest(".card")
   // console.log(cards)
 
   if (event.target.classList.contains("interview-btn")) {
+    // console.log("interview btn")
     cards.dataset.status = "interview";
     const statusButton = cards.querySelector(".status");
     // console.log(statusButton);
-    statusButton.innerText = 'interview';
+    statusButton.innerText = 'INTERVIEW';
   }
 
   if (event.target.classList.contains("rejected-btn")) {
+    // console.log("rejected btn")
     cards.dataset.status = "rejected";
     const statusButton = cards.querySelector(".status");
     // console.log(statusButton);
-    statusButton.innerText = "rejected";
+    statusButton.innerText = "REJECTED";
   }
 
 });
 
+// all filter button
 document.getElementById("all-btn")
   .addEventListener("click", function () {
     // console.log('button clicked');
@@ -43,11 +51,39 @@ document.getElementById("all-btn")
     }
   });
 
+  // interview count
+
+let countInterviewBtn = 0;
+
+const interviewButtons = document.querySelectorAll(".interview-btn");
+
+for (const buttons of interviewButtons) {
+  // console.log(buttons);
+  buttons.addEventListener("click", function () {
+    countInterviewBtn++;
+    interviewCount.innerText = countInterviewBtn;
+  });
+}
+
+// rejected count
+let countRejectedBtn = 0;
+const rejectedButtons = document.querySelectorAll(".rejected-btn");
+
+for (const buttons of rejectedButtons) {
+  // console.log(buttons);
+  buttons.addEventListener("click", function () {
+    countRejectedBtn++;
+    rejectedCount.innerText = countRejectedBtn;
+  });
+}
+
+// interview filter
+
 const hiddenDiv = document.getElementById('hidden-div');
 
 document.getElementById("interview-btn")
   .addEventListener("click", function () {
-    // console.log("interview clicked")
+    // console.log("rejected clicked")
     let count = 0;
     const allCards = document.querySelectorAll(".card");
     // console.log(allCards)
@@ -60,12 +96,15 @@ document.getElementById("interview-btn")
         cards.classList.add("hidden");
       }
     }
+    // hidden div
     if (count === 0) {
       hiddenDiv.classList.remove('hidden')
     } else {
       hiddenDiv.classList.add('hidden')
     }
   });
+
+  // rejected filter
 
 document.getElementById("rejected-btn")
   .addEventListener("click", function () {
@@ -82,12 +121,28 @@ document.getElementById("rejected-btn")
         cards.classList.add("hidden");
       }
     }
+    // hidden div
     if (count === 0) {
       hiddenDiv.classList.remove('hidden');
     } else {
       hiddenDiv.classList.add('hidden');
     }
   });
+
+  // delete btn
+jobsSectionCard.addEventListener("click", function (event) {
+  // console.log("clicked delete btn")
+  if (event.target.closest(".delete-btn")) {
+    // console.log("delete btn click")
+    const cards = event.target.closest(".card");
+    // console.log("cards")
+    cards.remove();
+    totalCount.innerText = jobsSectionCard.children.length
+  }
+});
+  
+
+  
 
 // console.log(hiddenDiv);
 // const cardsHidden = document.querySelectorAll(".card");
